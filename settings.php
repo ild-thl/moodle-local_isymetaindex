848314aabe651965a344fcbd84db4a378322f8a0
nessd <dness@dness.de>
2022-05-23 20:07:04 +0200

    zwischenstand

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@ if ( $hassiteconfig ){
     $ADMIN->add( 'localplugins', $settings );
 
     $settings->add( new admin_setting_configtext(
-        'local_isymetaindex/apikey', // ref
+        'local_isymetaindex/apikey', // wip reference to form elem
         'Externer API-Key',
         'This is the key used to access the External API',
         'Kein API-Key angegeben',
@@ -16,7 +16,7 @@ if ( $hassiteconfig ){
     ) );
 
     $settings->add( new admin_setting_configtext(
-        'local_isymetaindex/host', // ref
+        'local_isymetaindex/host', // wip reference to form elem
         'Externer Host',
         'This is the host used to access the External API',
         'Keinen Host angegeben',
